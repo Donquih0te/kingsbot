54f48f821a49673353ec9a2d5eefe78c21b66cb2
donquih0te <kb08ek@mail.ru>
2019-02-07 21:51:22 +0500

    Addition to friend button

--- src/main/java/ru/kingsbot/command/group/HelpCommand.java
+++ src/main/java/ru/kingsbot/command/group/HelpCommand.java
@@ -16,9 +16,8 @@ public class HelpCommand extends Command {
     public void execute(Player player, Integer peerId, Map<String, String> payload) {
         Utils.checkSignature(payload.get("key"), player.getId(), name);
         StringBuilder sb = new StringBuilder();
-        sb.append("Помощь по игре: ").append("vk.com/@kingsbot-pomosch-po-igre").append("\n")
-                .append("Помощь проекту: ").append("kingsbot.ru").append("\n\n")
-                .append("Если тебя пригласили в игру, то ты можешь написать \"реф <ID пригласившего>\" и вы оба получите небольшой бонус");
+        sb.append("Помощь по игре: ").append("https://vk.com/@kingsbot-pomosch-po-igre").append("\n")
+                .append("Помощь проекту: ").append("kingsbot.ru").append("\n\n");
 
         playerService.sendMessage(peerId, sb.toString(), null);
     }
