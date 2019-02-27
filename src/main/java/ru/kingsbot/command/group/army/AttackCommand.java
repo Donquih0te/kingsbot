bb7f7fc0a97942e2504085fc2608cdcb5423cb78
Donquih0te <kb08ek@mail.ru>
2019-02-28 00:36:06 +0500

    Return attacks

--- src/main/java/ru/kingsbot/command/group/army/AttackCommand.java
+++ src/main/java/ru/kingsbot/command/group/army/AttackCommand.java
@@ -1,11 +1,16 @@
 package ru.kingsbot.command.group.army;
 
+import ru.kingsbot.Emoji;
+import ru.kingsbot.attack.PlayerAttack;
 import ru.kingsbot.command.Command;
 import ru.kingsbot.entity.Player;
 import ru.kingsbot.utils.Utils;
 
 import java.text.SimpleDateFormat;
+import java.time.Instant;
+import java.util.Date;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 public class AttackCommand extends Command {
@@ -24,28 +29,28 @@ public class AttackCommand extends Command {
         Utils.checkSignature(payload.get("key"), player.getId(), name);
         StringBuilder sb = new StringBuilder();
         sb.append("Атаки отключены для устранения неполадок. В скором времени все вернется");
-//        Long currentTime = Instant.now().getEpochSecond();
-//        if(!players.containsKey(player.getId()) || (currentTime - players.get(player.getId()) > TIMESTAMP)) {
-//            List<Player> targets = playerService.getEnemyToAttack(player);
-//            Player target = Utils.getRandomValueFromList(targets);
-//            if(target == null) {
-//                sb.append(Emoji.RED_EXCLAMATION_MARK).append("Противников не найдено, попробуй позже");
-//            }else{
-//                if(target.getFirstName() == null) {
-//                    playerService.validateName(target);
-//                }
-//
-//                PlayerAttack attack = new PlayerAttack(player, target);
-//                attack.doAttack();
-//
-//                playerService.sendMessage(peerId, attack.getWhoResult(), null);
-//                playerService.sendMessage(target, attack.getTargetResult(), null);
-//                players.put(player.getId(), currentTime);
-//            }
-//        }else{
-//            sb.append(Emoji.RED_EXCLAMATION_MARK).append("Следующую атаку на королевство можно совершить через ")
-//                    .append(formatter.format(new Date((TIMESTAMP - (currentTime - players.get(player.getId()))) * 1000)));
-//        }
+        Long currentTime = Instant.now().getEpochSecond();
+        if(!players.containsKey(player.getId()) || (currentTime - players.get(player.getId()) > TIMESTAMP)) {
+            List<Player> targets = playerService.getEnemyToAttack(player);
+            Player target = Utils.getRandomValueFromList(targets);
+            if(target == null) {
+                sb.append(Emoji.RED_EXCLAMATION_MARK).append("Противников не найдено, попробуй позже");
+            }else{
+                if(target.getFirstName() == null) {
+                    playerService.validateName(target);
+                }
+
+                PlayerAttack attack = new PlayerAttack(player, target);
+                attack.doAttack();
+
+                playerService.sendMessage(peerId, attack.getWhoResult(), null);
+                playerService.sendMessage(target, attack.getTargetResult(), null);
+                players.put(player.getId(), currentTime);
+            }
+        }else{
+            sb.append(Emoji.RED_EXCLAMATION_MARK).append("Следующую атаку на королевство можно совершить через ")
+                    .append(formatter.format(new Date((TIMESTAMP - (currentTime - players.get(player.getId()))) * 1000)));
+        }
         playerService.sendMessage(peerId, sb.toString(), null);
     }
 }
