3b2a09c0e95f121cfeff5442a9868dd98d281055
Dinar <kb08ek@mail.ru>
2019-06-15 20:11:10 +0500

    Change configuration initializing
    1) Added creating configuration files

--- src/main/java/ru/kingsbot/Bot.java
+++ src/main/java/ru/kingsbot/Bot.java
@@ -3,8 +3,9 @@ package ru.kingsbot;
 import com.google.gson.Gson;
 import lombok.Getter;
 import lombok.extern.log4j.Log4j2;
+import org.apache.logging.log4j.core.config.Property;
 import ru.kingsbot.api.LongPool;
-import ru.kingsbot.api.VkApiClient;
+import ru.kingsbot.api.TransportClient;
 import ru.kingsbot.api.keyboard.Keyboards;
 import ru.kingsbot.attack.BossAttackMap;
 import ru.kingsbot.boss.BossMap;
@@ -13,21 +14,22 @@ import ru.kingsbot.service.*;
 import ru.kingsbot.utils.HibernateUtil;
 import ru.kingsbot.utils.Utils;
 
-import java.io.FileNotFoundException;
-import java.io.IOException;
-import java.io.InputStream;
+import java.io.*;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.List;
 import java.util.Properties;
 
 @Log4j2
 public class Bot {
 
-    private static final String PROPERTIES_FILE = "application.properties";
+    private static final String API_VERSION = "5.95";
+
     private static volatile Bot instance;
 
     @Getter
-    private VkApiClient vkApiClient;
-    @Getter
-    private RequestHandler requestHandler;
+    private TransportClient transportClient;
     @Getter
     private PlayerService playerService;
     @Getter
@@ -49,25 +51,39 @@ public class Bot {
     @Getter
     private Gson gson;
 
-    private String token;
-    private String version;
+    @Getter
+    private String dataPath;
+
+    @Getter
     private Integer groupId;
 
-    public Bot() throws FileNotFoundException {
+    public Bot(String dataPath) {
         instance = this;
-        gson = new Gson();
-        initProperties();
-        HibernateUtil.build();
-        
-        vkApiClient = new VkApiClient();
-        requestHandler = new RequestHandler(vkApiClient, token, version);
+        this.dataPath = dataPath;
 
-        playerService = new PlayerService(gson, requestHandler);
+        gson = new Gson();
+
+        // Checking the configuration files. If them does not exists then create default
+        try {
+            this.initPropertyFile("application.properties");
+            this.initPropertyFile("hibernate.properties");
+        } catch (FileNotFoundException e) {
+            log.error(e.getMessage(), e);
+            throw new RuntimeException(e);
+        }
+
+        // Initializing and starting the http-request executor
+        this.initTransportClient();
+        transportClient.start();
+
+        // Creating the database connection
+        HibernateUtil.build();
+        playerService = new PlayerService(gson, transportClient);
         marketService = new MarketService();
-        donateService = new DonateService();
+        //donateService = new DonateService();
         conversationService = new ConversationService();
 
-        longPool = new LongPool(this, requestHandler, groupId);
+        longPool = new LongPool(this, transportClient, groupId);
         bossMap = new BossMap();
         bossAttackMap = new BossAttackMap();
         commandMap = new CommandMap();
@@ -83,23 +99,44 @@ public class Bot {
     }
 
     public void shutdown() {
-        vkApiClient.close();
+        transportClient.close();
     }
 
-    private void initProperties() throws FileNotFoundException {
-        InputStream inputStream = Application.class.getClassLoader().getResourceAsStream(PROPERTIES_FILE);
-        if(inputStream == null) {
-            throw new FileNotFoundException(PROPERTIES_FILE + " not found");
+    private void initPropertyFile(String fileName) throws FileNotFoundException {
+        Path appPropPath = Paths.get(dataPath + fileName);
+        if(!Files.exists(appPropPath)) {
+            InputStream inputStream = this.getClass().getClassLoader().getResourceAsStream(fileName);
+            if(inputStream == null) {
+                throw new FileNotFoundException(String.format("The %s file doesn't exists in classpath", fileName));
+            }
+            try {
+                List<String> lines = Files.readAllLines(appPropPath);
+                Files.createFile(appPropPath);
+                Files.copy(inputStream, appPropPath);
+            } catch (IOException e) {
+                throw new RuntimeException(e);
+            }
         }
-        try{
-            Properties properties = new Properties();
-            properties.load(inputStream);
-            token = properties.getProperty("token");
-            version = properties.getProperty("version");
-            groupId = Utils.parseInt(properties.getProperty("group_id"));
-        }catch(IOException e) {
-            log.error("Failed to load properties", e);
+    }
+
+    private void initTransportClient() {
+        String appProp = "application.properties";
+        Properties prop = new Properties();
+        try {
+            prop.load(new FileReader(new File(dataPath + appProp)));
+        } catch (IOException e) {
+            log.error(e.getMessage(), e);
         }
+
+        groupId = Utils.parseInt(prop.getProperty("groupId"));
+
+        String token = prop.getProperty("token");
+
+        if(token == null) {
+            throw new RuntimeException("Specify a token in the " + appProp);
+        }
+
+        transportClient = new TransportClient(API_VERSION, token);
     }
 
 }
