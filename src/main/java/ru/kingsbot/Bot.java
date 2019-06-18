5485d533d7677d6577e5de077f0072d60eb1c1a0
Dinar <kb08ek@mail.ru>
2019-06-18 09:38:16 +0500

    Add bot version

--- src/main/java/ru/kingsbot/Bot.java
+++ src/main/java/ru/kingsbot/Bot.java
@@ -26,6 +26,7 @@ import java.util.Properties;
 @Log4j2
 public class Bot {
 
+    private static final String VERSION = "1.0.0-beta";
     private static final String API_VERSION = "5.95";
 
     private static volatile Bot instance;
