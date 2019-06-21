4dbc934106262c8e3c8d8e0a9874939aa8bb86e5
Dinar <kb08ek@mail.ru>
2019-06-22 01:14:15 +0500

    Add main wall objects
    Added objects that describe the user's post on the wall and comments to this post

---
+++ api/src/main/java/vk/sdk/objects/wall/WallComment.java
@@ -0,0 +1,84 @@
+package vk.sdk.objects.wall;
+
+import com.google.gson.annotations.SerializedName;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.ToString;
+
+import java.util.List;
+
+/**
+ *  Объект, описывающий комментарий к записи.
+ */
+@ToString
+@EqualsAndHashCode
+public class WallComment {
+
+    /**
+     *  Идентификатор комментария.
+     */
+    @Getter
+    @SerializedName("id")
+    private Integer id;
+
+    /**
+     *  Идентификатор автора комментария.
+     */
+    @Getter
+    @SerializedName("from_id")
+    private Integer fromId;
+
+    /**
+     *  Дата создания комментария в формате UnixTime.
+     */
+    @Getter
+    @SerializedName("date")
+    private Long date;
+
+    /**
+     *  Текст комментария.
+     */
+    @Getter
+    @SerializedName("text")
+    private String text;
+
+    /**
+     *  Идентификатор пользователя или сообщества, в ответ которому оставлен текущий комментарий (если применимо).
+     */
+    @Getter
+    @SerializedName("reply_to_user")
+    private Integer replyToUser;
+
+    /**
+     *  Идентификатор комментария, в ответ на который оставлен текущий (если применимо).
+     */
+    @Getter
+    @SerializedName("reply_to_comment")
+    private Integer replyToComment;
+
+    /**
+     *  Медиавложения комментария (фотографии, ссылки и т.п.)
+     *
+     *  TODO: create WallCommentAttachment objects to any type of attachments
+     */
+    @Getter
+    @SerializedName("attachments")
+    private List<Object> attachments;
+
+    /**
+     *  Массив идентификаторов родительских комментариев.
+     */
+    @Getter
+    @SerializedName("parents_stack")
+    private List<Integer> parentsStack;
+
+    /**
+     *  Информация о вложенной ветке комментариев.
+     *
+     *  TODO: create CommentThread object
+     */
+    @Getter
+    @SerializedName("thread")
+    private Object thread;
+
+}
