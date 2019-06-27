d40fc9526f1ae14cdd8056c6952f228971a084d3
Dinar <kb08ek@mail.ru>
2019-06-27 20:54:50 +0500

    Implement wiki page

---
+++ api/src/main/java/vk/sdk/objects/pages/PrivacySetting.java
@@ -0,0 +1,35 @@
+package vk.sdk.objects.pages;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+import lombok.AllArgsConstructor;
+import vk.sdk.objects.utils.EnumParam;
+
+@AllArgsConstructor
+public enum PrivacySetting implements EnumParam {
+
+    /**
+     *  Только руководители сообщества.
+     */
+    @JsonProperty("0")
+    COMMUNITY_MANAGERS_ONLY(0),
+
+    /**
+     *  Только участники сообщества.
+     */
+    @JsonProperty("1")
+    COMMUNITY_MEMBERS_ONLY(1),
+
+    /**
+     *  Просматривать страницу могут все.
+     */
+    @JsonProperty("2")
+    EVERYONE(2);
+
+    private final Integer value;
+
+    @Override
+    public String getValue() {
+        return value.toString();
+    }
+
+}
